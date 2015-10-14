2be12e7ebd9e250b83639acb7b2e0f6518acd558
Okan Kurtulus <okankurtulus@gmail.com>
2015-10-14 09:01:33 +0300

    Implemented completion block

--- Classes/EasyMailSender.h
+++ Classes/EasyMailSender.h
@@ -16,4 +16,6 @@ typedef void(^EasyMailComplete)(MFMailComposeViewController *controller, MFMailC
 + (instancetype)easyMail:(EasyMailBuilder) builder complete:(EasyMailComplete) complete;
 
 - (void)showFromViewController:(UIViewController *) viewController;
+- (void)showFromViewController:(UIViewController *) viewController competion:(void(^)(void))completion;
+
 @end
